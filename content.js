ef2f587e3962d2c474209e221ef56d442c81e056
getsbuffer <51249897+getsbuffer@users.noreply.github.com>
2023-08-28 23:09:06 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,13 @@
+console.log("Script has been loaded");
+broswer.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.command === 'toggleSpeed') {
+    var video = document.querySelector('video');
+    if (video) {
+      if (video.playbackRate === 2) {
+        video.palybackRate = 1;
+      } else {
+        video.playbackRate = 2;
+      }
+    }
+  }
+});
