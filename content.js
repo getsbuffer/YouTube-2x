4bb89a72dd5a4b2495bd7e7608fbbd9d4db51802
getsbuffer <51249897+getsbuffer@users.noreply.github.com>
2023-08-28 23:13:55 -0400

    Update content.js

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ broswer.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     var video = document.querySelector('video');
     if (video) {
       if (video.playbackRate === 2) {
-        video.palybackRate = 1;
+        video.playbackRate = 1;
       } else {
         video.playbackRate = 2;
       }
