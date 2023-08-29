55643f1e00650e74ab663ba7020ee07f25521fc3
guest271314 <guest271314@gmail.com>
2023-08-28 23:09:51 -0700

    Update content.js
    Substitute browser for broswer

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 console.log("Script has been loaded");
-broswer.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.command === 'toggleSpeed') {
     var video = document.querySelector('video');
     if (video) {
